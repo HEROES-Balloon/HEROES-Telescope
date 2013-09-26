40e119a8617493f257b2bd20c857f43ef5f3d0e1
Steven Christe <steven.d.christe@nasa.gov>
2013-09-26 14:16:08 -0400

    added routines to parse the xml track file from the CSBF website

---
+++ housekeeping/plot_GPSTrack.py
@@ -0,0 +1,28 @@
+import numpy as np
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+range = np.array([-2,2])
+lat_range = np.array([gps.latitude.max(), gps.latitude.min()]) + range
+lon_range = np.array([gps.longitude.max(), gps.longitude.min()]) + range
+lon_center = (lon_range[1] + lon_range[0]) * 0.5
+lat_center = (lat_range[1] + lat_range[0]) * 0.5
+
+plt.figure()
+
+m = Basemap(projection='stere',lon_0=lon_center,lat_0=lat_center,
+            llcrnrlat=lat_range[0],urcrnrlat=lat_range[1],
+            llcrnrlon=lon_range[0],urcrnrlon=lon_range[1],
+            resolution='i',area_thresh=10)
+
+m.drawcoastlines()
+m.drawstates()
+m.drawcounties()
+x1,y1=m(gps.longitude.values, gps.latitude.values)
+m.scatter(x1,y1,s=5,c='r',marker="o",alpha=1.0)
+parallels = np.arange(0.,90,1.)
+m.drawparallels(parallels,labels=[1,0,0,0],fontsize=10)
+meridians = np.arange(180.,360.,1.)
+m.drawmeridians(meridians,labels=[0,0,0,1],fontsize=10)
+plt.show()
