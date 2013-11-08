2f13ed747b59e1a9e7f5fde763178d59ae13c5e6
Steven Christe <steven.d.christe@nasa.gov>
2013-11-08 15:30:22 -0500

    Cleaned up time list

--- heroespy/util/times.py
+++ heroespy/util/times.py
@@ -3,16 +3,21 @@ A place to store all mission-significant times
 """
 
 from __future__ import absolute_import
-from datetime import datetime
+from sunpy.time import parse_time
 
-_year = 2013
-_month = 9
-_days = [21,22]
+launch = parse_time("2013/09/21 11:58:00.0")
+atfloat = parse_time("2013/09/21 14:49:00.0")
 
-launch = datetime(_year,_month,_days[0],11,58)
-float = datetime(_year,_month,_days[0],14,49)
-solarobs = (datetime(_year,_month,_days[0],15,21), datetime(_year,_month,_days[0],22,34))
-shutdown = datetime(_year,_month,_days[1],14,18)
+sas_pyasf_on = (parse_time("2013/09/21 15:21:04.896474"), parse_time("2013/09/21 22:35:54.476023"))
+sas_pyasr_on = (parse_time("2013/09/21 15:21:33.292299"), parse_time("2013/09/21 22:35:37.092514"))
+#ras_on = (parse_time("2013/09/21 14:49:00.0"), parse_time("2013/09/21 14:49:00.0"))
+
+sas_pyasr_wrongtarget = (parse_time("2013/09/21 14:49:00.0"), parse_time("2013/09/21 14:49:00.0"))
+
+solarobs_target1 = (parse_time("2013/09/21 15:22:00.00"), parse_time("2013/09/21 15:33:00.0"))
+solarobs_target2 = (parse_time("2013/09/21 15:35:00.0"), parse_time("2013/09/21 22:33:00.0"))
+
+shutdowm = parse_time("2013/09/22 14:18:00.0")
 
 # solar target command was received
-solar_target_change = (datetime(2013, 9, 21, 15, 33, 8), datetime(2013, 9, 21, 15, 33, 16))
+#solar_target_change = (datetime(2013, 9, 21, 15, 33, 8), datetime(2013, 9, 21, 15, 33, 16))
