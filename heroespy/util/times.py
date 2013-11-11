0d8d38a1744630ae38323ee9355d150bc4fa775c
Steven Christe <steven.d.christe@nasa.gov>
2013-11-11 16:21:57 -0500

    Added pyasr versus pyasf comparison notebook

--- heroespy/util/times.py
+++ heroespy/util/times.py
@@ -9,10 +9,12 @@ launch = parse_time("2013/09/21 11:58:00.0")
 atfloat = parse_time("2013/09/21 14:49:00.0")
 
 sas_pyasf_on = (parse_time("2013/09/21 15:21:04.896474"), parse_time("2013/09/21 22:35:54.476023"))
-sas_pyasr_on = (parse_time("2013/09/21 15:21:33.292299"), parse_time("2013/09/21 22:35:37.092514"))
+sas_pyasr_on = (parse_time("2013/09/21 15:21:33.292299"), parse_time("2013/09/21 18:55:00.00"), 
+                parse_time("2013/09/21 18:51:00.0000"), parse_time("2013/09/21 22:35:37.092514"))
+
 #ras_on = (parse_time("2013/09/21 14:49:00.0"), parse_time("2013/09/21 14:49:00.0"))
 
-sas_pyasr_wrongtarget = (parse_time("2013/09/21 14:49:00.0"), parse_time("2013/09/21 14:49:00.0"))
+sas_pyasr_wrongtarget = (parse_time("2013/09/21 18:49:00.0"), parse_time("2013/09/21 19:54:00.0"))
 
 solarobs_target1 = (parse_time("2013/09/21 15:22:00.00"), parse_time("2013/09/21 15:33:00.0"))
 solarobs_target2 = (parse_time("2013/09/21 15:35:00.0"), parse_time("2013/09/21 22:33:00.0"))
